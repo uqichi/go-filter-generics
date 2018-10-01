2dc432c9ba90e62edc2fc975dfd644047fc5c948
Tamura Yukichi <tamura.yukichi@gmail.com>
2018-10-02 00:17:38 +0900

    Add filterWithIface

--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	list := []int{1,2,3,4,5,4,3,2,1}
-	fmt.Println(filter(list, func(i int) bool {
+	ls := []int{1, 2, 3, 4, 5, 4, 3, 2, 1}
+
+	f := func(i int) bool {
 		if i == 3 {
 			return true
 		}
 		return false
-	}))
+	}
+
+	// expt. 1 filter plain
+	{
+		res := filter(ls, f)
+		fmt.Printf("res1: %+v\n", res)
+	}
+
+	// expt. 2 filter generics-like using interfaces
+	{
+		res := filterWithIface(ls, f)
+		resInt := res.([]int)
+		fmt.Printf("res2: %+v\n", resInt)
+	}
 }
 
 func filter(ls []int, f func(i int) bool) []int {
-	var ret []int
+	ret := make([]int, 0)
+
 	for _, v := range ls {
 		if f(v) {
 			ret = append(ret, v)
 		}
 	}
+
 	return ret
+}
+
+func filterWithIface(ls interface{}, f interface{}) interface{} {
+	lsVal := reflect.ValueOf(ls)
+	fVal := reflect.ValueOf(f)
+
+	ret := reflect.MakeSlice(reflect.TypeOf(ls), 0, lsVal.Len())
+
+	for i := 0; i < lsVal.Len(); i++ {
+		b := fVal.Call([]reflect.Value{lsVal.Index(i)})[0]
+		if b.Bool() {
+			ret = reflect.Append(ret, lsVal.Index(i))
+		}
+	}
+
+	return ret.Interface()
 }
